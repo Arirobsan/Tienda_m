eac0439ce65ab703bfef2697dd460b0c285189ac
arian <arian@Arianapc>
2024-03-13 17:47:40 -0600

    Semana 5: Mysql

---
+++ src/main/java/com/tienda_m/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package com.tienda_m.service;
+
+import com.tienda_m.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+
+    //Se define la firma del metodo para obtener los registros de categoria, en forma de lista
+    public List<Categoria> getCategorias(boolean activo);
+
+}
