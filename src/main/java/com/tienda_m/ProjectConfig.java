cb2e57c1bca876944fd39cd62fc2fb06bc3a3b9b
arian <arian@Arianapc>
2024-04-19 19:37:38 -0600

    Semana 13, carrito

--- src/main/java/com/tienda_m/ProjectConfig.java
+++ src/main/java/com/tienda_m/ProjectConfig.java
@@ -6,11 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -57,7 +54,12 @@ public class ProjectConfig implements WebMvcConfigurer {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((request)
                 -> request
-                        .requestMatchers("/", "/login", "/js/**", "/webjars/**","/registro/**")
+                        .requestMatchers("/",
+                                "/login",
+                                "/js/**",
+                                "/webjars/**",
+                                "/registro/**",
+                                "/carrito/**")
                         .permitAll()
                         .requestMatchers("/categoria/listado", "/producto/listado")
                         .hasRole("VENDEDOR")
